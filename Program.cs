acbb289d688f89b7f09913a02b6b76fbf84af1bc
Burhan <Burhan@DESKTOP-9NRFKA1>
2022-07-01 00:18:00 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Calling_Web_API_from_Console_Application_using_HttpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World! 1");
+            CallWebAPIAsync().Wait();
+
+            Console.WriteLine("******************************************");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("Hello World! 2 ");
+
+            CallWebAPIAsync2().Wait();
+
+            Console.WriteLine("******************************************");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("Hello World! 3 ");
+            //Program program = new Program();
+            //program.Disaply();
+        }
+        static async Task CallWebAPIAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".NET");
+                var data = await client.GetStringAsync("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139");
+                if (data != null)
+                {
+
+                    JObject jObject = JObject.Parse(data);
+                    Console.Write(jObject);
+                }
+                else
+                {
+                    Console.WriteLine("Internal server Error");
+                }
+            }
+        }
+        static async Task CallWebAPIAsync2()
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139");
+
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("User-Agent", ".NET");
+
+                HttpResponseMessage response = await client.GetAsync("");
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();                  
+                    JObject jObject = JObject.Parse(data);
+                    Console.Write(jObject);
+                }
+                else
+                {
+                    Console.WriteLine("Internal server Error");
+                }
+
+            }
+        }
+
+        //public async void Disaply()
+        //{
+        //    RootObject rootObject = await this.GetRequest();
+        //    Console.WriteLine("City name is" + rootObject.name);
+        //    Console.WriteLine("Temprature in K is" + rootObject.main.temp);
+        //}
+
+        //public async Task<RootObject> GetRequest()
+        //{
+        //    using (var client = new HttpClient())
+        //    {
+        //       try
+        //        {
+
+        //            var respose = await client.GetAsync(new Uri("https://fcc-weather-api.glitch.me/api/current?lat=35&lon=139"));
+        //            var result = await respose.Content.ReadAsStringAsync();
+        //            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+        //            var MemoryStream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+        //            var FinalData = serializer.ReadObject(MemoryStream) as RootObject;
+        //            return FinalData;
+        //        }
+        //        catch (Exception ex)
+        //        {
+
+        //            throw ex;
+        //        }
+                
+
+        //    }
+
+       // }
+
+    }
+}
---
+++ WeatherClass.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Web_API_from_Console_Application_using_HttpClient
+{
+    [DataContract]
+    public  class RootObject
+    {
+        [DataMember]
+        public coord coord { get; set; }
+        [DataMember]
+        public sys sys { get; set; }
+        [DataMember]
+        public main main { get; set; }
+        [DataMember]
+        public wind wind { get; set; }
+        [DataMember]
+        public List<weather>  weather { get; set; }
+
+        [DataMember]
+        public string @base { get; set; }
+        [DataMember]
+        public string visibility { get; set; }
+        [DataMember]
+        public string clouds { get; set; }
+        [DataMember]
+        public string dt { get; set; }
+        [DataMember]
+        public string timezone { get; set; }
+        [DataMember]
+        public string id { get; set; }
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string cod { get; set; }
+
+    }
+
+    [DataContract]
+    public class coord
+    {
+        [DataMember]
+        public int lon { get; set; }
+        [DataMember]
+        public int lat { get; set; }
+
+    }
+    [DataContract]
+
+    public class weather
+    {
+        [DataMember]
+        public int id { get; set; }
+        [DataMember]
+        public string main { get; set; }
+        [DataMember]
+        public string description { get; set; }
+        [DataMember]
+        public string icon { get; set; }
+
+    }
+    [DataContract]
+
+    public class main
+    {
+        [DataMember]
+        public string temp { get; set; }
+        [DataMember]
+        public string feels_like { get; set; }
+        [DataMember]
+        public string temp_min { get; set; }
+        [DataMember]
+        public string temp_max { get; set; }
+        [DataMember]
+        public string pressure { get; set; }
+        [DataMember]
+        public string humidity { get; set; }
+
+
+    }
+    [DataContract]
+
+    public class wind
+    {
+        [DataMember]
+        public string speed { get; set; }
+        [DataMember]
+        public string deg { get; set; }
+        [DataMember]
+        public string gust { get; set; }
+    }
+
+    [DataContract]
+    public class sys
+    {
+        [DataMember]
+        public string type { get; set; }
+        [DataMember]
+        public string id { get; set; }
+        [DataMember]
+        public string country { get; set; }
+        [DataMember]
+        public string sunrise { get; set; }
+        [DataMember]
+        public string sunset { get; set; }
+    }
+
+
+
+}
